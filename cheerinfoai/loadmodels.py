cb0f0156d68e229226711fffbca3aebaf72f0682
Patryk Dąbkowski <84743742+PatrickDabkowski@users.noreply.github.com>
2024-09-29 19:45:52 +0200

    quantized

--- cheerinfoai/loadmodels.py
+++ cheerinfoai/loadmodels.py
@@ -2,13 +2,17 @@ import torch
 from transformers import pipeline
 from transformers import BartTokenizer, BartForConditionalGeneration
 
-def load_bart(is_fast=True):
+def load_bart(is_fast=True, is_quant=False):
     tokenizer = BartTokenizer.from_pretrained('facebook/bart-base', is_fast=is_fast)
     model = BartForConditionalGeneration.from_pretrained('facebook/bart-base')
+    
+    if is_quant:
+        model = torch.ao.quantization.quantize_dynamic(model, dtype=torch.qint8) 
 
     return tokenizer, model
 
 def load_positive_classifier(device):
+    
     return pipeline("text-classification", device=device)
 
 
